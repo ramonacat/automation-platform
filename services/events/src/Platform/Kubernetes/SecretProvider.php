d39eab730c30fd80cecc2571f61804bc64586bc9
agares <ja@agares.info>
2021-07-21 15:52:27 +0200

    Use the correct exception when a secret is not readable

--- services/events/src/Platform/Kubernetes/SecretProvider.php
+++ services/events/src/Platform/Kubernetes/SecretProvider.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Ramona\AutomationPlatformSvcEvents\Platform\Kubernetes;
 
 use Ramona\AutomationPlatformSvcEvents\Platform\Secret;
+use Ramona\AutomationPlatformSvcEvents\Platform\SecretNotFoundException;
 use Ramona\AutomationPlatformSvcEvents\Platform\SecretProviderInterface;
+use Safe\Exceptions\FilesystemException;
 use function Safe\file_get_contents;
 
 final class SecretProvider implements SecretProviderInterface
@@ -17,8 +19,12 @@ final class SecretProvider implements SecretProviderInterface
 
     public function read(string $name): Secret
     {
-        $username = file_get_contents($this->basePath . '/' . $name . '/username');
-        $password = file_get_contents($this->basePath . '/' . $name . '/password');
+        try {
+            $username = file_get_contents($this->basePath . '/' . $name . '/username');
+            $password = file_get_contents($this->basePath . '/' . $name . '/password');
+        } catch (FilesystemException $e) {
+            throw SecretNotFoundException::forSecretName($name, $e);
+        }
 
         return new Secret($username, $password);
     }
--- services/events/src/Platform/SecretNotFoundException.php
+++ services/events/src/Platform/SecretNotFoundException.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ramona\AutomationPlatformSvcEvents\Platform;
 
+use Exception;
 use RuntimeException;
 use function sprintf;
 
@@ -13,8 +14,9 @@ final class SecretNotFoundException extends RuntimeException
      * @psalm-pure
      */
     public static function forSecretName(
-        string $secretName
+        string $secretName,
+        ?Exception $previous = null
     ): self {
-        return new self(sprintf('Secret "%s" does not exist', $secretName));
+        return new self(sprintf('Secret "%s" does not exist', $secretName), 0, $previous);
     }
 }
