06ace5958fc0be488a33c1ecd37db23566db02d4
agares <ja@agares.info>
2021-11-14 15:55:56 +0100

    [Tools/Build] Add a provider for rust targets

--- services/directory-watcher/build-config.php
+++ services/directory-watcher/build-config.php
@@ -5,15 +5,17 @@ use Ramona\AutomationPlatformLibBuild\Actions\RunProcess;
 use Ramona\AutomationPlatformLibBuild\Actions\PutFile;
 use Ramona\AutomationPlatformLibBuild\Actions\PutRuntimeConfiguration;
 use Ramona\AutomationPlatformLibBuild\BuildDefinition;
+use Ramona\AutomationPlatformLibBuild\Rust\TargetGenerator;
 use Ramona\AutomationPlatformLibBuild\Target;
 use Ramona\AutomationPlatformLibBuild\TargetId;
-use Ramona\AutomationPlatformLibBuild\Configuration\Configuration;
+
+$rustTargetGenerator = new TargetGenerator(__DIR__);
 
 $tag = str_replace('.', '', uniqid('', true));
 
 $imageWithTag =  'automation-platform-svc-directory-watcher:' . $tag;
 $migrationsImageWithTag =  'automation-platform-svc-migrations:' . $tag;
-$override = function (Configuration $configuration) use($imageWithTag, $migrationsImageWithTag) {
+$override = static function () use($imageWithTag, $migrationsImageWithTag):string {
     return <<<EOT
         apiVersion: apps/v1
         kind: Deployment
@@ -34,21 +36,27 @@ $override = function (Configuration $configuration) use($imageWithTag, $migratio
         EOT;
     };
 
-return new BuildDefinition([
-    new Target('build-dev', new Group([
-        new PutRuntimeConfiguration(__DIR__.'/runtime.configuration.json'),
-        new RunProcess('docker build -t ' . $imageWithTag . ' -f docker/Dockerfile ../../'),
-        new RunProcess('docker build -t ' . $migrationsImageWithTag . ' -f docker/migrations.Dockerfile .'),
-        new PutFile(__DIR__.'/k8s/overlays/dev/deployment.yaml', $override),
-    ]), [
-        new TargetId(__DIR__, 'clippy'),
-        new TargetId(__DIR__, 'fmt'),
-        new TargetId(__DIR__, 'tests-unit'),
-        new TargetId(__DIR__, 'unused-dependencies'),
-    ]),
-    new Target('clippy', new RunProcess('cargo clippy')),
-    new Target('tests-unit', new RunProcess('cargo test')),
-    new Target('deploy-dev', new RunProcess('kubectl --context minikube apply -k k8s/overlays/dev'), [new TargetId(__DIR__, 'build-dev'), new TargetId(__DIR__.'/../events/', 'deploy-dev')]),
-    new Target('fmt', new RunProcess('cargo fmt -- --check')),
-    new Target('unused-dependencies', new RunProcess('cargo +nightly udeps --all-targets')),
-]);
+return new BuildDefinition(
+    array_merge(
+        [
+            new Target(
+                'build-dev',
+                new Group(
+                    [
+                        new PutRuntimeConfiguration(__DIR__.'/runtime.configuration.json'),
+                        new RunProcess('docker build -t ' . $imageWithTag . ' -f docker/Dockerfile ../../'),
+                        new RunProcess('docker build -t ' . $migrationsImageWithTag . ' -f docker/migrations.Dockerfile .'),
+                        new PutFile(__DIR__.'/k8s/overlays/dev/deployment.yaml', $override),
+                    ]
+                ),
+                $rustTargetGenerator->targetIds()
+            ),
+            new Target(
+                'deploy-dev',
+                new RunProcess('kubectl --context minikube apply -k k8s/overlays/dev'),
+                [new TargetId(__DIR__, 'build-dev'), new TargetId(__DIR__.'/../events/', 'deploy-dev')]
+            ),
+        ],
+        $rustTargetGenerator->targets()
+    )
+);
---
+++ tools/build/src/Rust/TargetGenerator.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ramona\AutomationPlatformLibBuild\Rust;
+
+use function array_map;
+use Ramona\AutomationPlatformLibBuild\Actions\RunProcess;
+use Ramona\AutomationPlatformLibBuild\Target;
+use Ramona\AutomationPlatformLibBuild\TargetId;
+
+final class TargetGenerator
+{
+    /**
+     * @var non-empty-list<Target>
+     */
+    private array $targets;
+
+    public function __construct(private string $projectDirectory)
+    {
+        $this->targets = [
+            new Target('rust-clippy', new RunProcess('cargo clippy')),
+            new Target('rust-fmt-check', new RunProcess('cargo fmt -- --check')),
+            new Target('rust-tests-unit', new RunProcess('cargo test')),
+            new Target('rust-unused-dependencies', new RunProcess('cargo +nightly udeps --all-targets')),
+        ];
+    }
+
+    /**
+     * @return non-empty-list<Target>
+     */
+    public function targets(): array
+    {
+        return $this->targets;
+    }
+
+    /**
+     * @return non-empty-list<TargetId>
+     */
+    public function targetIds(): array
+    {
+        return array_map(fn (Target $t) => new TargetId($this->projectDirectory, $t->name()), $this->targets);
+    }
+}
