c2e071de21071cbb42a17754be64a46ac1d5c9a5
agares <ja@agares.info>
2021-11-22 23:50:16 +0100

    [All] Fix dependabot configuration
    1. It does not support wildcards in composer "path" repos, so replaced
    with the exact name at least for now
    2. Allegedly it should look for any file with "Dockerfile" in the name
    when doing "docker" updates (at least that's what the old issue here
    says: https://github.com/dependabot/feedback/issues/145)

--- tools/dependabot-config-checker/bin/check.php
+++ tools/dependabot-config-checker/bin/check.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 use Ramona\AutomationPlatformToolDependabotConfigChecker\Checker;
 use Ramona\AutomationPlatformToolDependabotConfigChecker\DefaultCheckerOutput;
-use Ramona\AutomationPlatformToolDependabotConfigChecker\DockerfileFinder;
 use Ramona\AutomationPlatformToolDependabotConfigChecker\ProjectFinder;
 use function Safe\file_get_contents;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dockerFiles = DockerfileFinder::find();
 $projects = ProjectFinder::find();
 
-$checker = new Checker(array_merge(['/'], $projects, $dockerFiles), new DefaultCheckerOutput());
+$checker = new Checker(array_merge(['/'], $projects), new DefaultCheckerOutput());
 
 exit($checker->validate(file_get_contents(__DIR__ . '/../../../.github/dependabot.yaml')));
