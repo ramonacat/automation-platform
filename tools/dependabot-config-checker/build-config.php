57fb112e0b76f097c9fb8307154140b81087fd94
agares <ja@agares.info>
2021-11-22 20:10:51 +0100

    [Tools/DependabotConfigChecker] Fix coding standard

--- tools/dependabot-config-checker/build-config.php
+++ tools/dependabot-config-checker/build-config.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Ramona\AutomationPlatformLibBuild\Actions\NoOp;
 use Ramona\AutomationPlatformLibBuild\BuildDefinition;
-use Ramona\AutomationPlatformLibBuild\PHP\TargetGenerator;
 use Ramona\AutomationPlatformLibBuild\PHP\Configuration;
+use Ramona\AutomationPlatformLibBuild\PHP\TargetGenerator;
 use Ramona\AutomationPlatformLibBuild\Target;
 
 $phpTargetGenerator = new TargetGenerator(__DIR__, new Configuration(minMsi: 53, minCoveredMsi: 66));
--- tools/dependabot-config-checker/ecs.php
+++ tools/dependabot-config-checker/ecs.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__.'/vendor/ramona/automation-platform-lib-coding-standard/ecs.php');
+    $containerConfigurator->import(__DIR__ . '/vendor/ramona/automation-platform-lib-coding-standard/ecs.php');
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
