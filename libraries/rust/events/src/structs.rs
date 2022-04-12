66d8887e305eb14ee14a932948dd49d196ded780
agares <ja@agares.info>
2022-04-12 21:26:54 +0200

    [Tools/MessageCompiler] Rename from EventCompiler

--- libraries/rust/events/src/structs.rs
+++ libraries/rust/events/src/structs.rs
@@ -1,28 +1,28 @@
 use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Metadata {
-    #[serde(with = "crate::system_time_serializer")]
-    pub created_time: std::time::SystemTime,
     pub id: ::uuid::Uuid,
     pub source: String,
+    #[serde(with = "crate::system_time_serializer")]
+    pub created_time: std::time::SystemTime,
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FileOnMountPath {
-    pub path: String,
     pub mount_id: String,
+    pub path: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 pub enum MessagePayload {
+    FileDeleted {
+        path: FileOnMountPath,
+    },
     FileMoved {
         from: FileOnMountPath,
         to: FileOnMountPath,
     },
     FileChanged {
-        path: FileOnMountPath,
-    },
-    FileDeleted {
         path: FileOnMountPath,
     },
     FileCreated {
