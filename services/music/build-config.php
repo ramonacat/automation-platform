27428c3aee2ff66fdedc55981c51538343d65161
agares <ja@agares.info>
2022-04-12 19:25:45 +0200

    [Services/Music] Create the music service

---
+++ services/music/build-config.php
@@ -0,0 +1,96 @@
+<?php
+
+use Ramona\AutomationPlatformLibBuild\Actions\Kubernetes\GenerateKustomizeOverride;
+use Ramona\AutomationPlatformLibBuild\Actions\Kubernetes\KustomizeApply;
+use Ramona\AutomationPlatformLibBuild\Actions\Kubernetes\KustomizeOverride;
+use Ramona\AutomationPlatformLibBuild\Actions\PutRuntimeConfiguration;
+use Ramona\AutomationPlatformLibBuild\Context;
+use Ramona\AutomationPlatformLibBuild\Definition\BuildDefinitionBuilder;
+use Ramona\AutomationPlatformLibBuild\Rust\TargetGenerator;
+use Ramona\AutomationPlatformLibBuild\Targets\DefaultTargetKind;
+use Ramona\AutomationPlatformLibBuild\Targets\TargetId;
+use Ramona\AutomationPlatformLibBuild\Docker\TargetGenerator as DockerTargetGenerator;
+
+return static function (BuildDefinitionBuilder $builder) {
+    $rustTargetGenerator = new TargetGenerator(__DIR__);
+
+    $builder->addTargetGenerator($rustTargetGenerator);
+
+    $builder->addTarget('put-runtime-config', new PutRuntimeConfiguration('runtime.configuration.json'));
+
+    $dockerTargetGenerator = new DockerTargetGenerator(
+        __DIR__,
+        'image-service',
+        'automation-platform-svc-music',
+        [
+            new TargetId(__DIR__, 'put-runtime-config')
+        ],
+        '../../',
+        'docker/Dockerfile'
+    );
+    $builder->addTargetGenerator($dockerTargetGenerator);
+
+    // todo this is copy-pasted from directory-watcher, there should be something more generalised for this
+    $builder->addTarget(
+        'generate-kustomize-override',
+        new GenerateKustomizeOverride(
+            'k8s/base/deployment.yaml',
+            'k8s/overlays/dev/',
+            [
+                new KustomizeOverride(
+                    '$.spec.template.metadata.labels.app',
+                    fn() => 'svc-music'
+                ),
+                new KustomizeOverride(
+                    '$.spec.template.spec.containers[0]',
+                    fn(Context $c) => [
+                        'name' => 'app',
+                        'image' => $c->artifactCollector()->getByKey(__DIR__, 'image-service')->name(),
+                        'volumeMounts' => [
+                            ...array_map(
+                                static fn(array $mount) => [
+                                    'name' => $mount['name'],
+                                    'mountPath' => '/mnt/' . $mount['name'],
+                                ],
+                                $c->configuration()->getSingleBuildValueOrDefault('$.kubernetes.mounts', [])
+                            )
+                        ]
+                    ]
+                ),
+                new KustomizeOverride(
+                    '$.spec.template.spec.volumes',
+                    fn(Context $c) => [
+                        ...array_map(
+                            static fn(array $mount) => [
+                                'name' => $mount['name'],
+                                'persistentVolumeClaim' => [
+                                    'claimName' => $mount['name'] . '--claim'
+                                ]
+                            ],
+                            $c->configuration()->getSingleBuildValueOrDefault('$.kubernetes.mounts', [])
+                        )
+                    ]
+                )
+            ]
+        ),
+        $dockerTargetGenerator->defaultTargetIds(DefaultTargetKind::Build),
+    );
+
+    $builder->addTarget(
+            'deploy',
+            new KustomizeApply('k8s/overlays/dev'),
+            [new TargetId(__DIR__.'/../events/', 'deploy')]
+    );
+
+    $builder->addDefaultTarget(
+        DefaultTargetKind::Build,
+        [
+            new TargetId(__DIR__, 'generate-kustomize-override'),
+            new TargetId(__DIR__.'/../../libraries/rust/platform/', 'build'),
+        ]
+    );
+
+    $builder->addDefaultTarget(
+        DefaultTargetKind::Fix
+    );
+};
