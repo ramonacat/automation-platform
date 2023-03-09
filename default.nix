d42b7443c291b3869613f463b1e0ad004fa220a1
Ramona Łuczkiewicz <ja@agares.info>
2023-03-09 18:10:35 +0100

    [Tools/Build] Docker image security scanning

--- default.nix
+++ default.nix
@@ -74,5 +74,6 @@ in
           cargo-udeps
           cargo-audit
           insta
+          grype
         ];
   }
--- services/music/docker/docker.nix
+++ services/music/docker/docker.nix
@@ -1,12 +1,11 @@
-{tag ? "latest"} @ args:
+{tag ? "latest", name} @ args:
     let
         pkgs = import <nixpkgs> { };
         crate = pkgs.callPackage ../Cargo.nix { };
     in
         pkgs.dockerTools.streamLayeredImage {
-            name = "svc-music";
+            inherit name tag;
             fromImage = null;
-            tag = tag;
             config.Cmd = [ 
                 "${crate.workspaceMembers.svc-music.build}/bin/svc-music" 
             ];
